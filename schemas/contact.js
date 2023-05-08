0eff548d270ff7837db127f23c400fc3d32a5d45
vishal-koundal <vishal2661999@gmail.com>
2023-05-08 19:42:06 +0530

    fix

--- schemas/contact.js
+++ schemas/contact.js
@@ -1,12 +1,22 @@
 export default {
   name: 'contact',
   type: 'document',
-  title: 'Pet',
+  title: 'Contact',
   fields: [
     {
       name: 'name',
       type: 'string',
       title: 'Name',
+    },
+    {
+      name: 'telephone',
+      type: 'number',
+      title: 'Phone Number',
+    },
+    {
+      name: 'message',
+      type: 'text',
+      title: 'Message',
     },
   ],
 }
