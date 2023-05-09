360383ded752f9df1544475ccc06ce08b03f3994
vishal-koundal <vishal2661999@gmail.com>
2023-05-09 16:45:27 +0530

    add customer

---
+++ schemas/customer.js
@@ -0,0 +1,39 @@
+export default {
+  name: 'customer',
+  title: 'Customer',
+  type: 'document',
+  fields: [
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+      readOnly: true,
+    },
+    {
+      name: 'password',
+      title: 'Password',
+      type: 'string',
+      //   hidden: true,
+    },
+    {
+      name: 'resetPasswordToken',
+      title: 'Reset Password Token',
+      type: 'string',
+      //   hidden: true,
+    },
+    {
+      name: 'status',
+      title: 'Status',
+      type: 'string',
+      options: {
+        list: ['active', 'notActive', 'banned'],
+        layout: 'dropdown',
+      },
+    },
+    {
+      name: 'fullName',
+      title: 'Full Name',
+      type: 'string',
+    },
+  ],
+}
--- schemas/index.js
+++ schemas/index.js
@@ -1,4 +1,5 @@
 import contact from './contact'
 import reservation from './reservation'
+import customer from './customer'
 
-export const schemaTypes = [contact, reservation]
+export const schemaTypes = [contact, reservation, customer]
